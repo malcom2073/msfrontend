6cf4a8ecce89bd54c177146f2e4181163bb744df
Michael Carpenter <malcom2073@gmail.com>
2020-10-24 19:06:39 -0400

    UI: Added more info to profile page, and added a redirect to /login on auth timeout

--- pages/profile.js
+++ pages/profile.js
@@ -5,6 +5,9 @@ import LoginForm from '../components/loginform'
 import nextCookie from 'next-cookies'
 import { privateRoute } from "../components/privateroute";
 import { render } from 'react-dom';
+import { create } from 'apisauce'
+import { AuthToken } from "../services/auth_token";
+import Router from 'next/router'
 
 class Profile extends React.Component {
     constructor(props)
@@ -27,9 +30,54 @@ class Profile extends React.Component {
           <Col>
           </Col>
       </Row>
+      <Row>
+      <Col>
+      Email: { (this.state.profile) ? (this.state.profile.email) : ("Loading...")}<br/>
+      Last IP : { (this.state.profile) ? (this.state.profile.lastip) : ("Loading...")}<br/>
+      
+      </Col>
+      </Row>
     </Container>
     </>
   )
+  }
+  async componentDidMount() {
+    console.log(this.state.auth);
+    var token = AuthToken.fromNext()
+    var headers = { Accept: 'application/vnd.github.v3+json'}
+    if (token) {
+        headers.Authorization = token.authorizationString();
+    }
+    const api = create({
+      baseURL: 'http://localhost:3000',
+      headers: headers,
+    })    
+    const response = await api.get('/api/userinfo');
+    console.log('UserInfoResponse');
+    console.log(response);
+    var profileobj = null;
+    // TODO: Handle more of these errors.
+    if (response.problem) {
+      switch (response.problem) {
+        case 'CLIENT_ERROR':
+          if (response.status == 401)
+          {
+            Router.push('/login?next=' + pathname)
+            //alert('Invalid credentials');
+            return {}
+            //Bad authentication!
+          }
+          break;
+        default:
+            break;
+      }
+      alert('Unknown error');
+    }
+    else {
+      profileobj = response.data.data;
+    }
+    //This is required to turn auth into an actual AuthToken instance, for passing into the component below.
+    this.setState({ auth: new AuthToken(this.props.auth.token) ,profile:profileobj })
   }
   static async getInitialProps(ctx) {
       //return ctx;
