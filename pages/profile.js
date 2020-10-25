92f5efac6793d4b6c5bfbb2bf0bf0fcb5b61440a
Michael Carpenter <malcom2073@gmail.com>
2020-10-24 21:11:14 -0400

    API: Broke API out into its own class

--- pages/profile.js
+++ pages/profile.js
@@ -5,8 +5,8 @@ import LoginForm from '../components/loginform'
 import nextCookie from 'next-cookies'
 import { privateRoute } from "../components/privateroute";
 import { render } from 'react-dom';
-import { create } from 'apisauce'
 import { AuthToken } from "../services/auth_token";
+import MsApi from '../lib/msapi';
 import Router from 'next/router'
 
 class Profile extends React.Component {
@@ -43,39 +43,8 @@ class Profile extends React.Component {
   }
   async componentDidMount() {
     console.log(this.state.auth);
-    var token = AuthToken.fromNext()
-    var headers = { Accept: 'application/vnd.github.v3+json'}
-    if (token) {
-        headers.Authorization = token.authorizationString();
-    }
-    const api = create({
-      baseURL: 'http://localhost:3000',
-      headers: headers,
-    })    
-    const response = await api.get('/api/userinfo');
-    console.log('UserInfoResponse');
-    console.log(response);
-    var profileobj = null;
-    // TODO: Handle more of these errors.
-    if (response.problem) {
-      switch (response.problem) {
-        case 'CLIENT_ERROR':
-          if (response.status == 401)
-          {
-            Router.push('/login?next=' + pathname)
-            //alert('Invalid credentials');
-            return {}
-            //Bad authentication!
-          }
-          break;
-        default:
-            break;
-      }
-      alert('Unknown error');
-    }
-    else {
-      profileobj = response.data.data;
-    }
+    var msapi = new MsApi();
+    var profileobj = await msapi.getUserInfo();
     //This is required to turn auth into an actual AuthToken instance, for passing into the component below.
     this.setState({ auth: new AuthToken(this.props.auth.token) ,profile:profileobj })
   }
