1066579ad440f5113d00053f2e90e18672b99c57
Michael Carpenter <malcom2073@gmail.com>
2020-11-03 21:42:15 -0500

    Admin: Started working through admin menu, and how it ties into the Admin Console

--- components/adminsidebar.js
+++ components/adminsidebar.js
@@ -2,70 +2,49 @@ import SideNav, { Toggle, NavItem, NavIcon, NavText } from '@trendmicro/react-si
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { faHome, faCogs, faTools, faUsers } from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link'
-
+import { UserOutlined, LaptopOutlined, NotificationOutlined, SettingFilled, DashboardOutlined, DatabaseOutlined} from '@ant-design/icons';
+import { ApiOutlined, MessageOutlined, TeamOutlined} from '@ant-design/icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Layout, Menu, Breadcrumb  } from 'antd';
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 class MSAdminSideBar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {isLoading: true};
     }
     render() {
-return (
-<SideNav
-    
-    onSelect={(selected) => {
-        // Add your code here
-    }}
->
-    <SideNav.Toggle />
-    <SideNav.Nav defaultSelected="home">
-        <NavItem eventKey="home">
-            <NavIcon>
-            <FontAwesomeIcon icon={faHome} style={{ fontSize: '1.75em' }}/>
-            </NavIcon>
-            <NavText>
-                Home
-            </NavText>
-        </NavItem>
-        <NavItem eventKey="config">
-            <NavIcon>
-            <FontAwesomeIcon icon={faTools} style={{ fontSize: '1.75em' }}/>
-            </NavIcon>
-            <NavText>
-                Configuration
-            </NavText>
-            <NavItem eventKey="config/general">
-                <NavText>
-                    <FontAwesomeIcon icon={faCogs} style={{ fontSize: '1.25em' }}/> <Link href="#">General Config</Link>
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="config/userlist">
-                <NavText>
-                    <FontAwesomeIcon icon={faUsers} style={{ fontSize: '1.25em' }}/> <Link href="/users">User List</Link>
-                </NavText>
-            </NavItem>
-        </NavItem>
-        <NavItem  eventKey="charts">
-            <NavIcon>
-            <FontAwesomeIcon icon={faTools} style={{ fontSize: '1.75em' }}/>
-            </NavIcon>
-            <NavText>
-                Configurations
-            </NavText>
-            <NavItem eventKey="charts/linechart">
-                <NavText>
-                    Line Chart
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="charts/barchart">
-                <NavText>
-                    Bar Chart
-                </NavText>
-            </NavItem>
-        </NavItem>
-    </SideNav.Nav>
-</SideNav>
-)
+        return (
+        <>
+            <Sider width={200} className="site-layout-background">
+                <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{ height: '100%', borderRight: 0 }}
+                >
+                <Menu.Item key="13" icon={<DashboardOutlined/>}>Dashboard</Menu.Item>
+                <SubMenu key="sub1" icon={<DatabaseOutlined />} title="Configuration">
+                    <Menu.Item key="1" icon={<SettingFilled />}>General settings</Menu.Item>
+                    <Menu.Item key="2" icon={<ApiOutlined/>}>API</Menu.Item>
+                    <Menu.Item key="3" icon={<MessageOutlined/>}>Messaging</Menu.Item>
+                    <Menu.Item key="4" icon={<TeamOutlined/>}>Users</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+                    <Menu.Item key="5">option5</Menu.Item>
+                    <Menu.Item key="6">option6</Menu.Item>
+                    <Menu.Item key="7">option7</Menu.Item>
+                    <Menu.Item key="8">option8</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+                    <Menu.Item key="9">option9</Menu.Item>
+                    <Menu.Item key="10">option10</Menu.Item>
+                    <Menu.Item key="11">option11</Menu.Item>
+                    <Menu.Item key="12">option12</Menu.Item>
+                </SubMenu>
+                </Menu>
+            </Sider>
+        </>
+        )
     }
 }
 export default MSAdminSideBar;
