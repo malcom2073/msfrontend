00436eb0e8425ac1f8ede7d0caf1b369b78fc79e
Michael Carpenter <malcom2073@gmail.com>
2020-11-04 22:10:50 -0500

    Formatting: Fixing space indentation

--- pages/profile.js
+++ pages/profile.js
@@ -10,72 +10,72 @@ import MsApi from '../lib/msapi';
 import pageLayout from '../components/pagelayout'
 
 class Profile extends React.Component {
-    constructor(props)
-    {
-        super(props);
-        this.state = props;
-    }
-  render() {
-      console.log(this.props);
-  return (
-    <>
-    <Container fluid>
-      <Row>
-          <Col>
-          </Col>
-          <Col>
-            <Table striped bordered hover>
-              <tbody>
-              <tr>
-                  <td>User</td>
-                  <td>{this.state.profile ? ( this.state.profile.name ) : ( 'No User' ) }</td>
-                </tr>
-                <tr>
-                  <td>Nickname</td>
-                  <td>{this.state.profile ? ( this.state.profile.nickname ) : ( 'Loading...' ) }</td>
-                </tr>
-                <tr>
-                  <td>Email</td>
-                  <td>{ (this.state.profile) ? (this.state.profile.email) : ("Loading...")}</td>
-                </tr>
-                <tr>
-                  <td>Last IP</td>
-                  <td>{ (this.state.profile) ? (this.state.profile.lastip) : ("Loading...")}</td>
-                </tr>
-                <tr>
-                  <td>Group</td>
-                  <td>{ (this.state.profile) ? (this.state.profile.primary_group.name) : ("Loading...")}</td>
-                </tr>
-                <tr>
-                  <td>Registered</td>
-                  <td>{ (this.state.profile) ? (this.state.profile.registered_date) : ("Loading...")}</td>
-                </tr>
-              </tbody>
-            </Table>
-          </Col>
-          <Col>
-          </Col>
-      </Row>
-      <Row>
-      <Col>
-      </Col>
-      </Row>
-    </Container>
-    </>
-  )
-  }
-  async componentDidMount() {
-    var msapi = new MsApi();
-    var profileobj = await msapi.getUserInfo();
-    //This is required to turn auth into an actual AuthToken instance, for passing into the component below.
-    this.setState({ auth: new AuthToken(this.props.auth.token) ,profile:profileobj })
-  }
-  static async getInitialProps(ctx) {
-      //return ctx;
-    //const navBar = await getUserNavbar(ctx)
-    //this.setState({navBar:navBar});
-    //return { navBar: navBar }
-  //  return {}
-  }
+		constructor(props)
+		{
+				super(props);
+				this.state = props;
+		}
+	render() {
+			console.log(this.props);
+	return (
+		<>
+		<Container fluid>
+			<Row>
+					<Col>
+					</Col>
+					<Col>
+						<Table striped bordered hover>
+							<tbody>
+							<tr>
+									<td>User</td>
+									<td>{this.state.profile ? ( this.state.profile.name ) : ( 'No User' ) }</td>
+								</tr>
+								<tr>
+									<td>Nickname</td>
+									<td>{this.state.profile ? ( this.state.profile.nickname ) : ( 'Loading...' ) }</td>
+								</tr>
+								<tr>
+									<td>Email</td>
+									<td>{ (this.state.profile) ? (this.state.profile.email) : ("Loading...")}</td>
+								</tr>
+								<tr>
+									<td>Last IP</td>
+									<td>{ (this.state.profile) ? (this.state.profile.lastip) : ("Loading...")}</td>
+								</tr>
+								<tr>
+									<td>Group</td>
+									<td>{ (this.state.profile) ? (this.state.profile.primary_group.name) : ("Loading...")}</td>
+								</tr>
+								<tr>
+									<td>Registered</td>
+									<td>{ (this.state.profile) ? (this.state.profile.registered_date) : ("Loading...")}</td>
+								</tr>
+							</tbody>
+						</Table>
+					</Col>
+					<Col>
+					</Col>
+			</Row>
+			<Row>
+			<Col>
+			</Col>
+			</Row>
+		</Container>
+		</>
+	)
+	}
+	async componentDidMount() {
+		var msapi = new MsApi();
+		var profileobj = await msapi.getUserInfo();
+		//This is required to turn auth into an actual AuthToken instance, for passing into the component below.
+		this.setState({ auth: new AuthToken(this.props.auth.token) ,profile:profileobj })
+	}
+	static async getInitialProps(ctx) {
+			//return ctx;
+		//const navBar = await getUserNavbar(ctx)
+		//this.setState({navBar:navBar});
+		//return { navBar: navBar }
+	//  return {}
+	}
 }
 export default privateRoute(pageLayout(Profile));
