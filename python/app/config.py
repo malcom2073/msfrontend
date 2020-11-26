5d207fa2eb42aa5da0fec09a6b81ee8ba2486da3
Michael Carpenter <malcom2073@gmail.com>
2020-11-25 20:11:16 -0500

    Tests: Added tests for /addForum API endpoint

--- python/app/config.py
+++ python/app/config.py
@@ -1,5 +1,6 @@
 
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///test.sqlite3'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///test.sqlite3'
+SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 SECRET_KEY = 'asdfklasjdfl;sahfdasjlkhfkjalrelka;sjdfl;sakhfdla;skhdfjklsa;jfdas'
--- python/tests/test_forums.py
+++ python/tests/test_forums.py
@@ -38,8 +38,31 @@ def test_forums_withgoodauth(client):
     jsonresponse = json.loads(rv.data)
     # Verify the password worked.
     assert jsonresponse['status'] == 'success'
-    assert False
 
+# Test inserting forums based on the forumindex variable above!
+def test_forums_insert(client):
+    # Grab a token and cookie
+    rv = client.post('/auth/auth',json={ 'username': 'Malcom', 'password': PASSWORD })
+    jsonresponse = json.loads(rv.data)
+    assert jsonresponse['status'] == 'success'
+    assert 'access_token' in jsonresponse
+    accesstoken = jsonresponse['access_token']
+    assert 'Set-Cookie' in rv.headers
+    cookie = rv.headers['Set-Cookie']
+    rv = client.get('/userinfo',headers={'Set-Cookie':cookie,'Authorization':'Bearer ' + accesstoken})
+    pprint.pprint(rv.data)
+    jsonresponse = json.loads(rv.data)
+    assert 'data' in jsonresponse
+    assert 'name' in jsonresponse['data'] and jsonresponse['data']['name'] == 'Malcom'
+    # We're good now to request to add forums!
+    for obj in forumindex:
+        rv = client.post('/addForum',
+            headers={'Set-Cookie':cookie,'Authorization':'Bearer ' + accesstoken},
+            json={'index':obj['id'],'title':obj['title'],'desc':obj['desc']})
+        jsonresponse = json.loads(rv.data)
+        assert 'status' in jsonresponse and jsonresponse['status'] == 'success'
+        pprint.pprint(rv.data)
+    #assert False
 
 # Test to make sure the index in the database matches the test forumindex
 def test_forum_index(client):
@@ -47,11 +70,18 @@ def test_forum_index(client):
     pprint.pprint(rv.data)
     jsonresponse = json.loads(rv.data)
     assert 'data' in jsonresponse and 'status' in jsonresponse and jsonresponse['status'] == 'success'
-    assert jsonresponse['data'] == forumindex
-    for obj in jsonresponse['data']:
-        for index in forumindex:
+    #assert jsonresponse['data'] == forumindex
+    for index in forumindex:
+        print(index)
+        found = False
+        for obj in jsonresponse['data']:
+            print("Object")
+            print(obj)
             if obj['id'] == index['id']:
                 assert obj['title'] == index['title']
                 assert obj['desc'] == index['desc']
+                found = True
+        assert found
+
         pprint.pprint(obj)
     assert False
