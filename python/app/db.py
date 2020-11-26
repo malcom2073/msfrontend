66bf81af161c2375646c4e125b88bcea325c2f80
Michael Carpenter <malcom2073@gmail.com>
2020-11-25 20:43:37 -0500

    Tests: Added tests for /addThread, to iterate through and make sure when we add them, they actually get added.

--- python/app/db.py
+++ python/app/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from . import config
 
-engine = create_engine(config.SQLALCHEMY_DATABASE_URI, echo = True)
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI, echo = False)
 Session = sessionmaker(bind = engine)
 session = Session() # Only valid in the main application thread!
 Model = declarative_base()
--- python/tests/test_forums.py
+++ python/tests/test_forums.py
@@ -7,19 +7,58 @@ import pprint
 forumindex = [
     {
         'id' : 1,
-        'title' : 'General Discussion',
-        'desc': 'General discussion forum, for general topics' 
+        'title' : "General Discussion",
+        'desc': "General discussion forum, for general topics"
     },
     {
         'id' : 2,
-        'title' : 'Support',
-        'desc': 'General support requests forum, for support for specific stuff' 
+        'title' : "Support",
+        'desc': "General support requests forum, for support for specific stuff"
     },
     {
         'id' : 3,
-        'title' : 'Suggestions',
-        'desc': 'Suggestion forum. Post your suggestions here!'
+        'title' : "Suggestions",
+        'desc': "Suggestion forum. Post your suggestions here!"
     }
+]
+
+topicindex = [
+    {
+        'id': 0,
+        'parent': 1,
+        'title': "This is the title text of the first general post",
+        'text':"This is the body text of the first general post!"
+    },
+    {
+        'id': 1,
+        'parent': 1,
+        'title': "This is the title text of the second general post",
+        'text':"This is the body text of the second general post!"
+    },
+    {
+        'id': 2,
+        'parent': 1,
+        'title': "This is the title text of the third general post",
+        'text':"This is the body text of the third general post!"
+    },
+    {
+        'id': 3,
+        'parent': 2,
+        'title': "This is the title text of the first support post",
+        'text':"This is the body text of the first support post!"
+    },
+    {
+        'id': 4,
+        'parent': 2,
+        'title': "This is the title text of the second support post",
+        'text':"This is the body text of the second support post!"
+    },
+    {
+        'id': 5,
+        'parent': 3,
+        'title': "This is the title text of the first suggestions post",
+        'text':"This is the body text of the first suggestions post!"
+    }    
 ]
 
 def test_forums_noauth(client):
@@ -64,6 +103,7 @@ def test_forums_insert(client):
         pprint.pprint(rv.data)
     #assert False
 
+
 # Test to make sure the index in the database matches the test forumindex
 def test_forum_index(client):
     rv = client.get('/getForumList')
@@ -84,4 +124,60 @@ def test_forum_index(client):
         assert found
 
         pprint.pprint(obj)
-    assert False
+
+# Test inserting posts based on the topicindex variable above!
+def test_forums_insert_posts(client):
+    # Grab a token and cookie
+    rv = client.post('/auth/auth',json={ 'username': 'Malcom', 'password': PASSWORD })
+    jsonresponse = json.loads(rv.data)
+    assert jsonresponse['status'] == 'success'
+    assert 'access_token' in jsonresponse
+    accesstoken = jsonresponse['access_token']
+    assert 'Set-Cookie' in rv.headers
+    cookie = rv.headers['Set-Cookie']
+    rv = client.get('/userinfo',headers={'Set-Cookie':cookie,'Authorization':'Bearer ' + accesstoken})
+    pprint.pprint(rv.data)
+    jsonresponse = json.loads(rv.data)
+    assert 'data' in jsonresponse
+    assert 'name' in jsonresponse['data'] and jsonresponse['data']['name'] == 'Malcom'
+    # We're good now to request to add forums!
+    for obj in topicindex:
+        rv = client.post('/addThread',
+            headers={'Set-Cookie':cookie,'Authorization':'Bearer ' + accesstoken},
+            json={'index':obj['id'],'parent':obj['parent'],'title':obj['title'],'text':obj['text']})
+        jsonresponse = json.loads(rv.data)
+        assert 'status' in jsonresponse and jsonresponse['status'] == 'success'
+        pprint.pprint(rv.data)
+    #assert False
+
+
+# Test to make sure the index in the database matches the test topicindex
+def test_forum_threads(client):
+    rv = client.get('/getForumList')
+    print("/getForumList response")
+    pprint.pprint(rv.data)
+    jsonresponse = json.loads(rv.data)
+    assert 'data' in jsonresponse and 'status' in jsonresponse and jsonresponse['status'] == 'success'
+    #assert jsonresponse['data'] == forumindex
+    foundcount = 0
+    for obj in jsonresponse['data']:
+        print("Forum" + str(obj))
+        rv = client.get('/getForumTopics?forumid=' + str(obj['id']))
+        jsonresponse = json.loads(rv.data)
+        print("/getForumTopics response")
+        pprint.pprint(rv.data)
+        assert 'status' in jsonresponse and jsonresponse['status'] == 'success' and 'data' in jsonresponse
+        for prethread in topicindex:
+            if prethread['parent'] == obj['id']:
+                found = False
+                for thread in jsonresponse['data']:
+                    if thread['id'] == prethread['id']:
+                        assert thread['title'] == prethread['title']
+                        assert thread['text'] == prethread['text']
+                        foundcount+=1
+                        found = True
+                if not found:
+                    pprint.pprint(prethread)
+                assert found
+    assert foundcount == len(topicindex)
+
