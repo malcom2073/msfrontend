362450b76c637fa7fd323dccca702b9fcfd55f34
Michael Carpenter <malcom2073@gmail.com>
2020-12-15 22:10:22 -0500

    Config: Changed back to file database

--- python/app/config.py
+++ python/app/config.py
@@ -1,7 +1,7 @@
 
 
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///test.sqlite3'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///test.sqlite3'
 # Used for in-memory database
-SQLALCHEMY_DATABASE_URI = 'sqlite://'
+#SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 SECRET_KEY = 'asdfklasjdfl;sahfdasjlkhfkjalrelka;sjdfl;sakhfdla;skhdfjklsa;jfdas'
