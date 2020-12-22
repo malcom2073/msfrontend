ceb949e38346293c9d58bdf5639d8b27d3bc77ce
Michael Carpenter <malcom2073@gmail.com>
2020-12-22 18:20:54 -0500

    Config: Fixed config to load CSS properly

--- next.config.js
+++ next.config.js
@@ -10,10 +10,18 @@ if (typeof require !== "undefined") {
 }
 
 module.exports = withCSS({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[local]___[hash:base64:5]",
+  //cssModules: true,
+  //cssLoaderOptions: {
+  //  importLoaders: 1,
+  //  localIdentName: "[local]___[hash:base64:5]",
+  //},
+  webpack: (config) => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: 'empty'
+    }
+
+    return config
   },
   ...withLess(
     withSass({
