8f146bf95398a03644d6adc26de6c96d0efd955a
Michael Carpenter <malcom2073@gmail.com>
2020-12-30 09:12:42 -0500

    Test: Added editor.js to try and narrow down css packaging issues

---
+++ pages/editor.js
@@ -0,0 +1,9 @@
+import dynamic from 'next/dynamic'
+
+const Editor = dynamic(import('../modules/forum/components/editor'), {ssr: false})
+export default function Edtior() {
+    console.log('Query');
+    return (
+    <Editor/>
+    )
+}
