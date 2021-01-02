fde446dbfe9ea09d286b639aa1a61c6501542ad3
Michael Carpenter <malcom2073@gmail.com>
2021-01-01 23:34:47 -0500

    Forums: Fixed bad export on comment test page

---
+++ pages/forums/topic/comment.js
@@ -0,0 +1,28 @@
+import {Container, Row, Col, Table} from 'react-bootstrap';
+import nextCookie from 'next-cookies'
+import { render } from 'react-dom';
+import pageLayout from '../../../components/pagelayout'
+import Forum_Index from '../../../components/forums'
+import CreateTopic from '../../../modules/forum/components/createtopic'
+import { privateRoute } from "../../../components/privateroute";
+class CommentCreate2 extends React.Component {
+		constructor(props)
+		{
+				super(props);
+				this.state = props;
+		}
+	render() {
+	return (
+		<>
+			Main Forum Index
+			Create Topic Here! ID: {this.props.query.forum}
+		<Container fluid>
+			<CreateTopic query={this.props.query}/>
+
+		</Container>
+		</>
+	)
+	}
+
+}
+export default privateRoute(pageLayout(CommentCreate2));
