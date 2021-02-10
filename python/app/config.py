bbff30d03cd18d9b152f73f46a873203512ebc29
Michael Carpenter <malcom2073@gmail.com>
2021-02-09 22:00:38 -0500

    Docker: Change docker env to use environment variables for configuration

--- python/app/config.py
+++ python/app/config.py
@@ -1,9 +1,13 @@
 
+import os
 
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+DB_HOST = os.getenv('DB_HOST')
+DB_DATABASE = os.getenv('DB_DATABASE')
+SECRET_KEY = os.getenv('BACKEND_SECRET_KEY')
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.sqlite3'
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@db/mikesshop'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://' + DB_USER + ':' + DB_PASS + '@' + DB_HOST + '/'+ DB_DATABASE
 
 # Used for in-memory database
 #SQLALCHEMY_DATABASE_URI = 'sqlite://'
-
-SECRET_KEY = 'asdfklasjdfl;sahfdasjlkhfkjalrelka;sjdfl;sakhfdla;skhdfjklsa;jfdas'
